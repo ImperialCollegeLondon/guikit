5ddf1eebb4c4e590431b3d94e9cfcfec3976e064
Diego <d.alonso-alvarez@imperial.ac.uk>
2021-10-14 11:10:26 +0100

    Add empty tests for scripts.py

---
+++ tests/test_scripts.py
@@ -0,0 +1,22 @@
+class TestRunSubCommand:
+    def test_add_arguments(self):
+        assert False
+
+    def test_run(self):
+        assert False
+
+
+class TestInitSubCommand:
+    def test_add_arguments(self):
+        assert False
+
+    def test_run(self):
+        assert False
+
+
+def test_parse_args():
+    assert False
+
+
+def test_main():
+    assert False

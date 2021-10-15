1d55386ee34824834b0eeba71e277c79ebca98ad
Diego <d.alonso-alvarez@imperial.ac.uk>
2021-10-15 10:27:24 +0100

    Use autoapi for the technical documentation

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,15 @@ release = "0.0.1"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["myst_parser", "sphinx.ext.napoleon"]
+extensions = [
+    "myst_parser",
+    "sphinx.ext.napoleon",
+    "autoapi.extension",
+    "sphinx.ext.autodoc",
+]
+autodoc_typehints = "description"
+autoapi_dirs = ["../pyguitemp"]
+autoapi_root = "technical/api"
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
@@ -52,7 +60,7 @@ source_suffix = {
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "alabaster"
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,

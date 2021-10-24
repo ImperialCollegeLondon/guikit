66dc1cc02d1a4f7c87e94c43f2aaf64e3cd602e6
Diego <d.alonso-alvarez@imperial.ac.uk>
2021-10-24 21:17:31 +0100

    Bump version: 0.1.6 → 0.1.7

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = "2021, Research Computing Service, Imperial College London"
 author = "Research Computing Service, Imperial College London"
 
 # The full version, including alpha/beta/rc tags
-release = "0.1.6"
+release = "0.1.7"
 
 # -- General configuration ---------------------------------------------------
 
--- pyguitemp/__init__.py
+++ pyguitemp/__init__.py
@@ -1,4 +1,4 @@
 from pathlib import Path
 
-VERSION = "0.1.6"
+VERSION = "0.1.7"
 APP_NAME = Path(__file__).parent.stem

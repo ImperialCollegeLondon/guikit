0ab9c536719ca24cf99442e03a77810e53fc5c5d
Diego <d.alonso-alvarez@imperial.ac.uk>
2021-10-24 20:35:41 +0100

    Bump version: 0.1.2 → 0.1.3

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = "2021, Research Computing Service, Imperial College London"
 author = "Research Computing Service, Imperial College London"
 
 # The full version, including alpha/beta/rc tags
-release = "0.1.2"
+release = "0.1.3"
 
 # -- General configuration ---------------------------------------------------
 

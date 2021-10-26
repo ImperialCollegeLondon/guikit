55532cc0da2a2203db7bbbb648749417cc75e6a9
Diego <d.alonso-alvarez@imperial.ac.uk>
2021-10-26 22:06:11 +0100

    :sparkles: Adds copying existing plugins

--- pyguitemp/skeleton/__init__.py
+++ pyguitemp/skeleton/__init__.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
-from pyguitemp.logging import Logger
+from pyguitemp.logging import logger
 
 VERSION = "0.0.1"
 APP_NAME = Path(__file__).parent.stem
-logger = Logger(APP_NAME)
+logger.app_name = APP_NAME

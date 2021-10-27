884e89deb40f4aa7e6a4ce1551765db19b464e57
Diego <d.alonso-alvarez@imperial.ac.uk>
2021-10-27 05:43:47 +0100

    Add load data example

---
+++ pyguitemp/extensions/load_data/model.py
@@ -0,0 +1,17 @@
+import pandas as pd
+from pubsub import pub
+
+
+def load_data(filename: str) -> None:
+    """Loads data from disk.
+
+    Args:
+        filename: Name of the file to load. Must contain data in CSV format.
+    """
+    try:
+        data = pd.read_csv(filename)
+    except Exception as err:
+        data = None
+        filename = f"No data could be loaded. {err.args}"
+
+    pub.sendMessage("data.load", filename=filename, data=data)
--- pyguitemp/extensions/load_data/presenter.py
+++ pyguitemp/extensions/load_data/presenter.py
@@ -2,10 +2,23 @@ from typing import List
 
 from pyguitemp.plugins import PluginBase, Tab
 
-from .view import DataLoaderTab
+from .model import load_data
+from .view import DataLoaderTab, FileDialogCustom
 
 
 class DataPlugin(PluginBase):
     def tabs(self, parent=None) -> List[Tab]:
-        data_loader_tab = DataLoaderTab(parent)
+        data_loader_tab = DataLoaderTab(parent, select_file)
         return [Tab(page=data_loader_tab, text="Data", order=0)]
+
+
+def select_file() -> None:
+    """Loads a text file from disk."""
+    with FileDialogCustom() as dlg:
+        if not dlg.open():
+            # the user changed their mind
+            return
+
+        filename = dlg.GetPath()
+
+    load_data(filename)

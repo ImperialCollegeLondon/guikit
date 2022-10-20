391da374f8d224b0f85d0debb45733b000ae6905
Diego <d.alonso-alvarez@imperial.ac.uk>
2022-10-20 10:59:57 +0100

    Bump version: 0.2.3 → 0.2.4

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = "2021, Research Computing Service, Imperial College London"
 author = "Research Computing Service, Imperial College London"
 
 # The full version, including alpha/beta/rc tags
-release = "0.2.3"
+release = "0.2.4"
 
 # -- General configuration ---------------------------------------------------
 
--- guikit/__init__.py
+++ guikit/__init__.py
@@ -1,4 +1,4 @@
 from pathlib import Path
 
-VERSION = "0.2.3"
+VERSION = "0.2.4"
 APP_NAME = Path(__file__).parent.stem

aa5111e3dc6bfa557d8a6976edea62d887282441
Alex Dewar <a.dewar@imperial.ac.uk>
2022-12-05 10:49:03 +0000

    Don't explicitly set logging level anywhere
    The default logging level -- which should be "info", provided the user
    hasn't changed it -- should be fine.
    
    Setting the logging level to "debug" unfortunately causes extremely
    verbose logging for some Python modules and you can end up getting a lot
    of log spam. For example, I'm trying to use the azure package in another
    project and the contents of all the requests are being output to the
    command line.

--- guikit/logging.py
+++ guikit/logging.py
@@ -66,7 +66,6 @@ class Logger:
         """
         if not isinstance(self._logger, logging.Logger):
             self._logger = logging.getLogger("root")
-            self._logger.setLevel(logging.DEBUG)
 
             self.set_console_handler()
             self.set_file_handler()
@@ -79,7 +78,6 @@ class Logger:
     def set_console_handler(self):
         """Sets a handler to print the log to the terminal."""
         ch = logging.StreamHandler()
-        ch.setLevel(logging.DEBUG)
 
         # create and add formatter to handle
         formatter = logging.Formatter(
@@ -99,7 +97,6 @@ class Logger:
 
         filename = log_path / f"{datetime.now().strftime('%Y%m%d_%H-%M-%S')}.log"
         ch = logging.FileHandler(str(filename))
-        ch.setLevel(logging.INFO)
 
         # create and add formatter to handle
         formatter = logging.Formatter(
